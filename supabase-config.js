035e073abfbe1ad7863b783a7530b0eab74c9f83
chenghanchen <chenghanchen1@gmail.com>
2026-08-03 07:16:34 -0500

    Deploy Ting’s Snack House website

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.TINGS_SUPABASE = {
+  url: 'https://ragqunnuxsfwhrfqpylg.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhZ3F1bm51eHNmd2hyZnFweWxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3MzM3NTEsImV4cCI6MjEwMTMwOTc1MX0.Y-1ibC7kLj6aJnmFUPgm60hS6acDcwIPuNNo1ByQERk'
+};
